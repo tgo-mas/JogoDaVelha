d3b2bcd348eeb92d375eb69a5d3ad2bda03f3839
Thomas Almeida <99335340+tgo-mas@users.noreply.github.com>
2022-10-18 07:53:16 -0300

    Add funcao jogadaPc()

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-
+/// Coleta de elementos do DOM
 const quadro1 = document.getElementById('0');
 const quadro2 = document.getElementById('1');
 const quadro3 = document.getElementById('2');
@@ -15,10 +15,11 @@ const placarX = document.getElementById('placarX');
 
 var quadros = [quadro1, quadro2, quadro3, quadro4, quadro5, quadro6, quadro7, quadro8, quadro9];
 
-
+//// Variáveis para funcionamento do game
 var player = "X";
 var fim = false;
 
+/// escolherQuadro(id) -> Seleciona o quadro escolhido para o player da vez (var player).
 function escolherQuadro(i){
     console.log(i);
     if(fim){
@@ -29,16 +30,18 @@ function escolherQuadro(i){
     }
     quadros[i].classList.add('marcado');
     quadros[i].innerHTML = player;
-    verificarPlacar(i);
+    verificarGame();
     mudarJogador();
 }
 
+/// limparQuadro(id) -> Limpa o quadro 'id'. Chamado na função restart().
 function limparQuadro(quadro){
     quadro.classList.remove('marcado');
     quadro.classList.remove('venceu');
     quadro.innerHTML = '.';
 }
 
+/// mudarJogador() -> Muda o jogador da vez (var player).
 function mudarJogador() {
     if (player === "X"){
         player = "O";
@@ -48,7 +51,8 @@ function mudarJogador() {
     jogador.innerHTML = player;
 }
 
-function verificarPlacar(i) {
+/// verificarGame() -> Verifica o jogo para saber se há vencedor.
+function verificarGame() {
     for(let i = 0; i < 3; i++){
         if(quadros[i].innerHTML == 'X' && quadros[i + 3].innerHTML == "X" && quadros[i + 6].innerHTML == "X" || 
         quadros[i].innerHTML == 'O' && quadros[i + 3].innerHTML == "O" && quadros[i + 6].innerHTML == "O"){
@@ -92,6 +96,81 @@ function verificarPlacar(i) {
     }
 }
 
+/// jogadaPc() -> Verifica o jogo e faz uma jogada estratégica (ofensiva ou defensiva). Usado no modo Player vs. PC.
+function jogadaPc(){
+    let jogou = false;
+
+    //----------condicionais ofensivas-----------
+    if(((jogo[1][0] == 'O' && jogo[2][0] == 'O') || (jogo[0][1] == 'O' && jogo[0][2] == 'O') || (jogo[1][1] == 'O' && jogo[2][2] == 'O')) && (jogo[0][0] == 1)){
+        jogo[0][0] = 'O';
+        return true;
+    }else if(((jogo[0][0] == 'O' && jogo[0][2] == 'O') || (jogo[1][1] == 'O' && jogo[2][1] == 'O')) && (jogo[0][1] == 2)){
+      jogo[0][1] = 'O';
+      return True;
+    }else if (((jogo[0][0] == 'O' && jogo[0][1] == 'O') || (jogo[1][2] == 'O' && jogo[2][2] == 'O') || (jogo[1][1] == 'O' && jogo[2][0] == 'O')) && (jogo[0][2] == 3)){
+      jogo[0][2] = 'O';
+      return True;
+    }else if( ((jogo[0][0] == 'O' && jogo[2][0] == 'O') || (jogo[1][1] == 'O' && jogo[1][2] == 'O')) && (jogo[1][0] == 4)){
+      jogo[1][0] = 'O';
+      return True;
+    }else if(((jogo[0][0] == 'O' && jogo[2][2] == 'O') || (jogo[0][1] == 'O' && jogo[2][1] == 'O') || (jogo[1][0] == 'O' && jogo[1][2] == 'O') || (jogo[0][1] == 'O' && jogo[2][1] == 'O')) && (jogo[1][1] == 5)){
+      jogo[1][1] = 'O';
+      return True;
+    }else if( ((jogo[0][2] == 'O' && jogo[2][2] == 'O') || (jogo[1][0] == 'O' && jogo[1][1] == 'O')) && (jogo[1][2] == 6)){
+      jogo[1][2] = 'O';
+      return True;
+    }else if( ((jogo[0][0] == 'O' && jogo[1][0] == 'O') || (jogo[0][2] == 'O' && jogo[1][1] == 'O') || (jogo[2][1] == 'O' && jogo[2][2] == 'O')) && (jogo[2][0] == 7)){
+      jogo[2][0] = 'O';
+      return True;
+    }else if( ((jogo[0][1] == 'O' && jogo[1][1] == 'O') || (jogo[2][0] == 'O' && jogo[2][2] == 'O')) && (jogo[2][1] == 8)){
+      jogo[2][1] = 'O';
+      return True;
+    }else if( ((jogo[0][2] == 'O' && jogo[1][2] == 'O') || (jogo[2][0] == 'O' && jogo[2][1] == 'O') || (jogo[0][0] == 'O' && jogo[1][1] == 'O')) && (jogo[2][2] == 9)){
+      jogo[2][2] = 'O';
+      return True;
+    }
+
+    //----- condicionais defensivas------
+    if(((jogo[1][0] == 'X' && jogo[2][0] == 'X') || (jogo[0][1] == 'X' && jogo[0][2] == 'X') || (jogo[1][1] == 'X' && jogo[2][2] == 'X')) && (jogo[0][0] == 1)){
+      jogo[0][0] = 'O';
+      return True;
+    }else if( ((jogo[0][0] == 'X' && jogo[0][2] == 'X') || (jogo[1][1] == 'X' && jogo[2][1] == 'X')) && (jogo[0][1] == 2)){
+      jogo[0][1] = 'O';
+      return True;
+    }else if( ((jogo[0][0] == 'X' && jogo[0][1] == 'X') || (jogo[1][2] == 'X' && jogo[2][2] == 'X') || (jogo[1][1] == 'X' && jogo[2][0] == 'X')) && (jogo[0][2] == 3)){
+      jogo[0][2] = 'O';
+      return True;
+    }else if( ((jogo[0][0] == 'X' && jogo[2][0] == 'X') || (jogo[1][1] == 'X' && jogo[1][2] == 'X')) && (jogo[1][0] == 4)){
+      jogo[1][0] = 'O';
+      return True;
+    }else if( ((jogo[0][0] == 'X' && jogo[2][2] == 'X') || (jogo[0][1] == 'X' && jogo[2][1] == 'X') || (jogo[1][0] == 'X' && jogo[1][2] == 'X') || (jogo[0][1] == 'X' && jogo[2][1] == 'X')) && (jogo[1][1] == 5)){
+      jogo[1][1] = 'O';
+      return True;
+    }else if( ((jogo[0][2] == 'X' && jogo[2][2] == 'X') || (jogo[1][0] == 'X' && jogo[1][1] == 'X')) && (jogo[1][2] == 6)){
+      jogo[1][2] = 'O';
+      return True;
+    }else if( ((jogo[0][0] == 'X' && jogo[1][0] == 'X') || (jogo[0][2] == 'X' && jogo[1][1] == 'X') || (jogo[2][1] == 'X' && jogo[2][2] == 'X')) && (jogo[2][0] == 7)){
+      jogo[2][0] = 'O';
+      return True;
+    }else if( ((jogo[0][1] == 'X' && jogo[1][1] == 'X') || (jogo[2][0] == 'X' && jogo[2][2] == 'X')) && (jogo[2][1] == 8)){
+      jogo[2][1] = 'O';
+      return True;
+    }else if( ((jogo[0][2] == 'X' && jogo[1][2] == 'X') || (jogo[2][0] == 'X' && jogo[2][1] == 'X') || (jogo[0][0] == 'X' && jogo[1][1] == 'X')) && (jogo[2][2] == 9)){
+      jogo[2][2] = 'O';
+      return True;
+    }
+    while (!jogou){
+      i = randint(0,2);
+      j = randint(0,2);
+  
+      if(jogo[i][j] != 'X' && jogo[i][j] != 'O'){
+        jogo[i][j] = 'O';
+        jogou = True;  
+      }
+    }
+}
+
+/// endGame() -> Termina o jogo e declara um vencedor (ou velha).
 function endGame(){
     switch(player){
         case 'X':
@@ -107,6 +186,7 @@ function endGame(){
     fim = true;
 }
 
+/// restart() -> Chama limparQuadro(id) para todos os quadros.
 function restart(){
     for(let quadro of quadros){
         limparQuadro(quadro);
